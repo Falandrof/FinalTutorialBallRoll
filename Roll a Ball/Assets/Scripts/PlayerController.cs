dff855312ab61941395654f200194c4e6fecfe2b
Dylan Cranford <dylan.cranford173@gmail.com>
2019-07-09 20:54:33 -0400

    Challenge Commit

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,49 +1,74 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+using System;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
 
     public float speed;
     public Text countText;
     public Text winText;
+    public Text scoreText;
 
     private Rigidbody rb;
     private int count;
+    private int score;
 
-    void Start()
+    void Start ()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
-        SetCountText();
+        score = 0; 
+        SetCountText ();
+        SetScoreText ();
         winText.text = "";
     }
 
-    void FixedUpdate()
+    void FixedUpdate ()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce(movement * speed);
+        rb.AddForce (movement * speed);
     }
 
-    void OnTriggerEnter(Collider other)
+
+    void OnTriggerEnter(Collider other) 
     {
-        if (other.gameObject.CompareTag("Pick Up"))
+        if (other.gameObject.CompareTag ( "Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            score = score + 1; // I added this code to track the score and count separately.
+            
+            SetCountText();
+            SetScoreText();
+            
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
         {
             other.gameObject.SetActive(false);
-            count = count + 1;
+            count = count + 1; 
+            score = score - 1; // this removes 1 from the score
             SetCountText();
+            SetScoreText();
+    }
+}
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if (count == 12)
+        {
+            transform.position = new Vector3(40f, 10f, 13.0f); 
         }
     }
 
-    void SetCountText()
+ void SetScoreText ()
     {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 12)
+        scoreText.text = "Score: " + score.ToString ();
+        if (score >= 22)
         {
             winText.text = "You Win!";
         }
